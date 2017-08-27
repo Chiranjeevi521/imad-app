a99d5f5dbbacda9d616646618717bdba5baee1e8
Chiranjeevi521 <chiranjeevi521@gmail.com>
2017-08-27 21:19:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,27 @@ app.get('/article-one', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/articles/:articleName', function(req, res){
+    
+    //var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title = article-one", function(err, result){
+        
+        if (err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length === 0){
+                
+                res.status(404).send('Article Not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+        
+    });
+});
+
 app.get('/article-two', function(req, res){
     
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
