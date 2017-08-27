6d3f4e75d4829a3a719a6c4e334fcbd7725892c6
Chiranjeevi521 <chiranjeevi521@gmail.com>
2017-08-27 21:21:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/articles/:articleName', function(req, res){
     
     //var articleName = req.params.articleName;
     
-    pool.query("SELECT * FROM article WHERE title = article-one", function(err, result){
+    pool.query("SELECT * FROM article WHERE title = 'article-one'", function(err, result){
         
         if (err){
             res.status(500).send(err.toString());
