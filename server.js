487b0b55daf9d21c31341af5ffa69ad9387a4e77
Chiranjeevi521 <chiranjeevi521@gmail.com>
2017-09-10 23:28:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/check-login', function (req, res) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              console.log('You are logged in')
+              console.log('You are logged in');
               res.send(result.rows[0].username);    
            }
        });
